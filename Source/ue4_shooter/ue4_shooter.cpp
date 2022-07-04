066c9a4c6a5994c35cbcd1d585acfe05707a4666
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-07-05 02:20:39 +0300

    Initial commit

---
+++ Source/ue4_shooter/ue4_shooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ue4_shooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue4_shooter, "ue4_shooter" );
